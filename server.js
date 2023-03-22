a5c73a7eaeff22ae4e42b0206d291863e41e3b83
Karn Yongsiriwit <palinda.j64@gmail.com>
2023-03-22 16:15:16 +0700

    update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
   })
 })
 
-app.get('/customer', function(req, res) {
+app.get('/customers', function(req, res) {
   connection.query(
     'SELECT * FROM a1_customer',
     function(err, results) {
@@ -41,7 +41,7 @@ app.get('/order', function(req, res) {
   )
 })
 
-app.get('/product', function(req, res) {
+app.get('/products', function(req, res) {
   connection.query(
     'SELECT * FROM a1_product',
     function(err, results) {
@@ -50,6 +50,67 @@ app.get('/product', function(req, res) {
     }
   )
 })
+
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get("/top_customer", function (req, res) {
+  connection.query(
+    `SELECT 
+    C.name, 
+    SUM(O.qtt*P.price) AS price_sum 
+  FROM a1_customer AS C 
+    INNER JOIN a1_order AS O ON C.Cid = O.Cid 
+    INNER JOIN a1_product AS P ON O.Pid = P.Pid 
+  GROUP BY 
+    C.Cid
+  ORDER BY 
+    price_sum DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get('/top_products', function(req, res){
+  connection.query(
+    `SELECT O.Oid, P.Pname, O.qtt, SUM(O.qtt) as Total_QTY FROM a1_order as O INNER JOIN a1_product as P ON O.Pid = P.Pid GROUP BY O.Oid, P.Pname, O.qtt, P.price ORDER BY Total_QTY DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+
+app.post("/createusers", function (req, res) {
+  const name = req.body.name;
+  const surname = req.body.surname;
+  const email = req.body.email;
+  const city = req.body.city;
+  const tel = req.body.tel;
+  connection.query(
+    `INSERT INTO a1_customer (name, surname, email, city,  tel) VALUES (?, ?, ?, ?, ? ,? ,?)`,
+    [name, surname, email, city, tel],
+    function (err, results) {
+      if (err) {
+        res.json(err);
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.post('/orders', function(req, res) {
+  const values = req.body
+  console.log(values)
+  connection.query(
+    'INSERT INTO a1_order (Oid, Pid, qtt, Cid) VALUES ?', [values],
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
 
 app.listen(5000, () => {
   console.log('Server is started.')
